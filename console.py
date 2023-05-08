20459ed775bd262ed6fe9f0ee8f281e1120be81b
pacmanlolo <michael.omotosho7@gmail.com>
2023-05-08 07:05:57 -0700

    Prisc & Pacman

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for the entry point of the command interpreter."""
+"""This module for the entry point of the command interpreter."""
 
 import cmd
 from models.base_model import BaseModel
